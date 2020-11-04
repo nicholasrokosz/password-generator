7d96a18dbfe188079a8c1e7136f43db60b74f6c2
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-11-03 17:52:21 -0700

    generatePassword() is functional

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// generatePassword definition
+function generatePassword() {
+
+  const lower = 'abcdefghijklmnopqrstuvwxyz'
+  const upper = lower.toUpperCase()
+  const numeric = '1234567890'
+  const special = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~" // <-- ???????????? " and \
+  let chars = ''
+  let password = ''
+
+  passLength = parseInt(
+    prompt("How long should your password be?\nEnter a whole number between 8 and 128 inclusive:")
+  )
+  console.log(passLength);
+
+  if (passLength < 8 || passLength > 128 || passLength === NaN) { // <-- ??????????????
+    alert("That isn't a whole number between 8 and 128");
+    return;
+  }
+  var includeLower = confirm("Select ok to include lowercase characters:")
+  if (includeLower) {
+    chars += lower;
+  }
+  var includeUpper = confirm("Select ok to include uppercase characters:")
+  if (includeUpper) {
+    chars += upper;
+  }
+  var includeNumeric = confirm("Select ok to include numeric characters:")
+  if (includeNumeric) {
+    chars += numeric;
+  }
+  var includeSpecial = confirm("Select ok to include special characters:")
+  if (includeSpecial) {
+    chars += special;
+  }
+  if (!includeLower && !includeUpper && !includeNumeric && !includeSpecial) {
+    alert("You must include at least one type of character");
+    return;
+  }
+  for (i = 0; i < passLength; i++) {
+    password += chars[Math.floor(Math.random() * chars.length)]
+  }
+  console.log(password);
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
